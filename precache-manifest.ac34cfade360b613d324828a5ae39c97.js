8404d63d0a6a42e21c98acc5bdd852045d1402a0
scyfw1 <scyfw1@nottingham.ac.uk>
2024-09-15 16:18:09 +0100

    Updates

---
+++ precache-manifest.ac34cfade360b613d324828a5ae39c97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058cba00dea31419425e79740c17c7f1",
+    "url": "/PersonalCV-New/index.html"
+  },
+  {
+    "revision": "d259596008b378d47f6b",
+    "url": "/PersonalCV-New/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0c9e113b74f76df2e203",
+    "url": "/PersonalCV-New/static/js/2.ba3c2162.chunk.js"
+  },
+  {
+    "revision": "b9ce7c648ef22fd661baca594e74d173",
+    "url": "/PersonalCV-New/static/js/2.ba3c2162.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23149536f32e96e2a405",
+    "url": "/PersonalCV-New/static/js/3.31b14468.chunk.js"
+  },
+  {
+    "revision": "d259596008b378d47f6b",
+    "url": "/PersonalCV-New/static/js/main.8938dec2.chunk.js"
+  },
+  {
+    "revision": "e78d322d9907707f6d10",
+    "url": "/PersonalCV-New/static/js/runtime-main.e5852b06.js"
+  }
+]);
